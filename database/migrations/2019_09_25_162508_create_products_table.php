73152bec4429c5f1319bcd12a85a02beab30f25d
drunkking <dusan.kuburic@outlook.com>
2019-09-25 18:44:19 +0200

    middleware setup

--- database/migrations/2019_09_25_162508_create_products_table.php
+++ database/migrations/2019_09_25_162508_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('role_id')->default(3)->unsinged();
-            $table->rememberToken();
+            $table->string('vendor');
+            $table->mediumText('description');
+            $table->integer('price');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <h2>All products</h2>
+
+@endsection
